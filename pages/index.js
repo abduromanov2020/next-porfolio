f1b995e05c5063ebf49416742797279b90c4a49c
abduromanov2020 <abduromanov2020@gmail.com>
2023-01-24 21:21:10 +0700

    fix: typo import services

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import About from "../components/UI/About";
 import Contact from "../components/UI/Contact";
 import Hero from "../components/UI/Hero";
 import Portfolio from "../components/UI/Portfolio";
-import Services from "../components/UI/services";
+import Services from "../components/UI/Services";
 import Testimonial from "../components/UI/Testimonial";
 
 export default function Home() {
